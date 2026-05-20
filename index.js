93f447d1707a2ef4cd6682fc891f52594617290e
Rungrot Muangchalerm <rungrot.muangchalerm@gmail.com>
2026-05-20 16:17:19 +0700

    feat: api register

--- index.js
+++ index.js
@@ -2,12 +2,21 @@ const express = require('express')
 const app = express()
 const port = 3000
 app.use(express.static('public'))
+
 app.get('/api/me', (req, res) => {
     res.status(200).json({
         status: "200",
         role: "user"
     })
 })
+
+app.post('/api/register', (req, res) => {
+    res.status(200).json({
+        status: "200",
+        message: "success"
+    })
+})
+
 app.listen(port, () => {
     console.log(`http://localhost:${port}`)
 })
