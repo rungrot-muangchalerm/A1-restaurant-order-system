59d85c19d2b0dc71284b33d70e572831ea3098ff
Aumandanna <karsumeth627@gmail.com>
2026-05-20 18:31:53 +0700

    chore: del debug

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -5,25 +5,19 @@ fetch('/api/me').then(res => res.json()).then(data => {
             document.getElementById('cashier').classList.remove('d-none')
             document.getElementById('kitchen').classList.remove('d-none')
             document.getElementById('pos').classList.remove('d-none')
-            console.log(data)
         }
         else
             if (data.role === 'pos') {
                 document.getElementById('pos').classList.remove('d-none')
-                console.log(data.role)
             }
             else
                 if (data.role == 'kitchen') {
                     document.getElementById('kitchen').classList.remove('d-none')
-                    console.log(data.role)
                 }
                 else
                     if (data.role == 'cashier') {
                         document.getElementById('cashier').classList.remove('d-none')
-                        console.log(data.role)
                     }
-
-
     }
     else {
         console.log(`status err ${data}`);
