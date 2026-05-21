38ee330a5fcdb4539c4f58e4fc074acbf7efb832
rungrot-muangchalerm <rungrot.muangchalerm@gmail.com>
2026-05-22 00:19:07 +0700

    feat: การเช็คสิทธ์การแสดงผลของ navbar ตาม role ของ api และ สถานะ login lououts

---
+++ public/assets/js/script.js
@@ -0,0 +1,20 @@
+fetch('/api/auth/me').then(res => res.json()).then(data => {
+    console.log(data)
+    if (data.status === '200') {
+        document.getElementById('login').classList.add('d-none')
+        document.getElementById('register').classList.add('d-none')
+
+        if (data.role === 'admin') {
+            document.getElementById('POS').classList.remove('d-none')
+            document.getElementById('kitchen').classList.remove('d-none')
+            document.getElementById('cashier').classList.remove('d-none')
+            document.getElementById('admin').classList.remove('d-none')
+        } else if (data.role === 'POS') {
+            document.getElementById('POS').classList.remove('d-none')
+        } else if (data.role === 'kitchen') {
+            document.getElementById('kitchen').classList.remove('d-none')
+        }
+    } else {
+        console.log(data)
+    }
+})
