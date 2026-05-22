6fea764fc7318682177c0182f9452d432b7d598d
rungrot-muangchalerm <rungrot.muangchalerm@gmail.com>
2026-05-23 05:07:47 +0700

    feat: เพิ่ม sharedContainer แสดงเมนูกับข้าว และวางโครงหมวดอื่น

--- public/assets/js/customer/menu.js
+++ public/assets/js/customer/menu.js
@@ -32,6 +32,9 @@ fetch('/api/menu').then(res => res.json()).then(data => {
 
         const singleDishContainer = document.getElementById('singleDish-container')
         const singleDishTemplate = document.getElementById('singleDish-template')
+
+        const sharedContainer = document.getElementById('shared-container')
+        const sharedTemplate = document.getElementById('shared-template')
         data.menu.forEach(element => {
             if (element.category === 'อาหารจานเดียว') {
                 const clone = singleDishTemplate.content.cloneNode(true)
@@ -41,6 +44,24 @@ fetch('/api/menu').then(res => res.json()).then(data => {
                 clone.querySelector('[data-role="price"]').textContent = element.price + ' บาท'
                 clone.querySelector('[data-role="category"]').textContent = element.category
                 singleDishContainer.appendChild(clone)
+            } else if (element.category === 'กับข้าว') {
+                const clone = sharedTemplate.content.cloneNode(true)
+                clone.querySelector('[data-role="img"]').src = element.Image
+                clone.querySelector('[data-role="category"]').textContent = element.category
+                clone.querySelector('[data-role="name"]').textContent = element.name
+                clone.querySelector('[data-role="description"]').textContent = element.discription
+                clone.querySelector('[data-role="price"]').textContent = element.price + ' บาท'
+                sharedContainer.appendChild(clone)
+            } else if (element.category === 'เส้นและแกง') {
+                console.log('c')
+            } else if (element.category === 'ของทานเล่น') {
+                console.log('d')
+            } else if (element.category === 'เครื่องดื่ม') {
+                console.log('e');
+            } else if (element.category === 'ของหวาน') {
+                console.log('d');
+            } else if (element.category === 'ชุดเซ็ต') {
+                console.log('ไม่เจอเหี้ยไรเลย')
             }
         })
     }
